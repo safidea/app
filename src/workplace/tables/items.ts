2c855c786f7bb23b0155d69c9305df63a24f09e5
Thomas JEANNEAU <thomas@essentiel.io>
2024-07-29 22:48:31 +0200

    chore(workplace): update tables

--- src/workplace/tables/items.ts
+++ src/workplace/tables/items.ts
@@ -6,34 +6,51 @@ export const items: Table = {
     {
       name: 'activity',
       field: 'LongText',
+      required: true,
     },
     {
       name: 'quantity',
       field: 'Number',
+      required: true,
     },
     {
       name: 'unity',
       field: 'SingleLineText',
+      required: true,
     },
     {
       name: 'unit_price',
       field: 'Number',
+      required: true,
     },
     {
       name: 'vat',
       field: 'Number',
+      required: true,
     },
     {
       name: 'total_vat',
-      field: 'Number',
+      field: 'Formula',
+      formula: 'quantity * unit_price * vat / 100',
+      output: {
+        field: 'Number',
+      },
     },
     {
       name: 'total_et',
-      field: 'Number',
+      field: 'Formula',
+      formula: 'quantity * unit_price',
+      output: {
+        field: 'Number',
+      },
     },
     {
       name: 'total_ati',
-      field: 'Number',
+      field: 'Formula',
+      formula: 'total_et + total_vat',
+      output: {
+        field: 'Number',
+      },
     },
   ],
 }
--- src/workplace/tables/times.ts
+++ src/workplace/tables/times.ts
@@ -6,26 +6,34 @@ export const times: Table = {
     {
       name: 'customer',
       field: 'SingleLineText',
+      required: true,
     },
     {
       name: 'project',
       field: 'SingleLineText',
+      required: true,
     },
     {
       name: 'hours',
-      field: 'Number',
+      field: 'SingleSelect',
+      options: ['0', '1', '2', '3', '4'],
+      required: true,
     },
     {
       name: 'minutes',
-      field: 'Number',
+      field: 'SingleSelect',
+      options: ['0', '15', '30', '45'],
+      required: true,
     },
     {
       name: 'content',
       field: 'LongText',
+      required: true,
     },
     {
       name: 'date',
       field: 'DateTime',
+      required: true,
     },
   ],
 }
